5bb41b054a11f8fb4f6ec9d0fcd4283772e68ad1
Goutham Kumar Adunuri <77602631+Goutham6172@users.noreply.github.com>
2023-06-04 22:33:46 -0500

    working api

---
+++ src/test/java/com/rewards/app/RewardsCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.rewards.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RewardsCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
